43cf777b7242da9cf7fd49d12c1536653562e9d1
Ankit <ankitrajdihara123@gmail.com>
2026-05-07 17:55:52 +0530

    file_important

--- build-server/script.js
+++ build-server/script.js
@@ -1,16 +1,20 @@
 const { exec } = require("child_process");
 const path = require("path");
+require("dotenv").config();
 const fs = require("fs");
-const { S3Client, PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const mime = require("mime-types");
+const PROJECT_ID = process.env.PROJECT_ID;
 const s3Client = new S3Client({
-  region: "Asia Pacific (Mumbai) ap-south-1",
+  region: "ap-south-1",
   credentials: {
-    accessKeyId: process.env.accessKeyId,
-    secretAccessKey: process.env.secretAccessKey,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
   },
 });
 async function init() {
+  console.log(process.env.ACCESS_KEY_ID);
+  console.log(process.env.SECRET_ACCESS_KEY);
   console.log("Executing Script.js");
   const outDirPath = path.join(__dirname, "output");
   const p = exec(`cd ${outDirPath} && npm install && npm run build`);
@@ -28,16 +32,18 @@ async function init() {
     });
 
     for (const filePath of distFolderContent) {
-      if (fs.lstatSync(filePath).isDirectory) continue;
-      console.log("uploading file path ");
+      const fullPath = path.join(distFolderPath, filePath);
+      if (fs.lstatSync(fullPath).isDirectory()) continue;
+      console.log("Uploading:", filePath);
       const command = new PutObjectCommand({
         Bucket: "vercel-out",
         Key: `__outputs/${PROJECT_ID}/${filePath} `,
-        Body: fs.createReadStream(filePath),
-        ContentType: mime.lookup(filePath),
+        Body: fs.createReadStream(fullPath),
+        ContentType: mime.lookup(fullPath),
       });
       await s3Client.send(command);
     }
     console.log("done");
   });
 }
+init();
